2bddfa39cbf9287a0fc28017a80af8d5f695b700
Raymond Yang <raymond.yang@centrapay.com>
2023-07-29 18:58:04 +1200

    Create GameFileCSVReader class

--- games/datareader/csvdatareader.py
+++ games/datareader/csvdatareader.py
@@ -1,6 +1,6 @@
 import os
 import csv
-
+from games.domainmodel.models import Game, Publisher, Genre
 
 class GameFileCSVReader:
 
@@ -23,5 +23,11 @@ class GameFileCSVReader:
         return self.__dataset_of_genres
 
     def read_csv_file(self):
-        # TODO
-        pass
+      with open(self.__filename) as csv_file:
+          csv_reader = csv.DictReader(csv_file, delimiter=',')
+          for row in csv_reader:
+              self.__dataset_of_games.append(Game(int(row["AppID"]), row["Name"]))
+              self.__dataset_of_publishers.add(Publisher(row["Publishers"]))
+              genre_names = row["Genres"].split(",")
+              for genre_name in genre_names:
+                  self.__dataset_of_genres.add(Genre(genre_name))
